504a9b19dc887860cdd6695b9674b3ae075b4bcf
HyoJeong Moon <57999419+kot8585@users.noreply.github.com>
2023-05-15 21:38:36 +0900

    스토리북 controls addon 설치

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,6 +10,7 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-controls",
   ],
   framework: {
     name: "@storybook/nextjs",
