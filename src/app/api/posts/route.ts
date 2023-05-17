0a3bdea2396fcd515faeb9bc61f22e4682584b8d
HyoJeong Moon <57999419+kot8585@users.noreply.github.com>
2023-05-17 11:16:33 +0900

    getServerSession에 idx가 안나오니 email로 idx 조회하는걸로 대체

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -2,6 +2,7 @@ import { getAllPostList, getFollowingPostList } from "@/service/post";
 import { Session, getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import { handler } from "../auth/[...nextauth]/route";
+import { getIdxByEmail } from "@/service/user";
 
 // http://localhost:3000/api/posts?feedType=${feedType}
 export async function GET(req: NextRequest, res: NextResponse) {
@@ -12,18 +13,13 @@ export async function GET(req: NextRequest, res: NextResponse) {
     if (!session) {
       throw new Error("로그인이 되어있지 않음");
     }
-    const userIdx = session.user.idx;
-    console.log("session", session);
-    console.log("userIdx: " + userIdx);
-    return (
-      getFollowingPostList(userIdx)
-        // getAllPosList()
-        .then((res) => NextResponse.json(res))
-        .catch((error) => {
-          console.error(error);
-          return new Response(JSON.stringify(error), { status: 500 });
-        })
-    );
+    const userIdx = await getIdxByEmail(session.user.email!);
+    return getFollowingPostList(userIdx)
+      .then((res) => NextResponse.json(res))
+      .catch((error) => {
+        console.error(error);
+        return new Response(JSON.stringify(error), { status: 500 });
+      });
   }
 
   return getAllPostList()
