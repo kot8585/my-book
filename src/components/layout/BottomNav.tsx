dbf70b43a9421de4b46068cae2d170fa2bbfbd4e
HyoJeong Moon <57999419+kot8585@users.noreply.github.com>
2023-05-18 02:30:50 +0900

    컴포넌트 파일 구조 변경

--- src/components/layout/BottomNav.tsx
+++ src/components/layout/BottomNav.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { menu } from "../topNav/TopNav";
+import { menu } from "./TopNav";
 import { TiHome } from "react-icons/ti";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
--- src/components/layout/TopNav.stories.tsx
+++ src/components/layout/TopNav.stories.tsx
@@ -1,10 +1,17 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import TopNav from "./TopNav";
+import AuthProvider from "../../provider/AuthProvider";
 
 const meta: Meta<typeof TopNav> = {
   title: "Components/TopNav",
   component: TopNav,
-  argTypes: {},
+  decorators: [
+    (Story) => (
+      <AuthProvider>
+        <Story />
+      </AuthProvider>
+    ),
+  ],
 };
 
 export default meta;
